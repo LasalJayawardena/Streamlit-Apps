1bfcb7947b88416aa995f31573d27cfc5004ab26
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-03-08 19:41:57 +0530

    initila commit

---
+++ Motor_Accidents_Dashboard/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:81e41f5dabc197f15ff40fca0b4a527955a86442c863bfd246d68413aed6662b
+size 3439
---
+++ Sentimental_Analysis_Dashboard/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b58b6a089c5967f8cb70c9c7610520655ae1680f028e11c5d456a17f7bd5b5b7
+size 3573
